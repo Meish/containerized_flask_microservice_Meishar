02a0b710b0182865e5279ce58f1f9fffcab4a879
Meishar Shahoha <meish123@gmail.com>
2022-02-04 10:01:50 +0200

    first commit

--- Makefile
+++ Makefile
@@ -1,1 +1,15 @@
+make install:
+	pip install --upgrade pip &&
+		pip install -r requirements.txt
 
+make lint: 
+	echo linting 
+
+make format:
+	echo formating 
+
+make test:
+	echo testing
+
+make all:
+	install lint format test
--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+from flask import Flask, request, jsonify
+from flask.logging import create_logger
+import logging
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    """Return a friendly HTTP greeting."""
+    print("I am inside hello world")
+    return 'Hello World! I can make change at route: /change'
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
