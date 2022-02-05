bacf1018b6cd826a125b282a26f2122da23c054c
Meishar Shahoha <meish123@gmail.com>
2022-02-05 18:48:30 +0200

    fixed test_app

--- test_app.py
+++ test_app.py
@@ -7,7 +7,7 @@ def test_add():
     assert(addition(1,2) == 3)
     assert(addition(3,2) == 5)
     assert(addition(1,5) == 6)
-    assert(addition(150,-32) == 128)
+    assert(addition(150,-32) == 118)
 
 def test_multiply():
     assert(multiply(1,2) == 2)
