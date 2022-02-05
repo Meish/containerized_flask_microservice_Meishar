e798ad1b3423e1b1960354777a5fb58c0a39d6be
Meishar Shahoha <meish123@gmail.com>
2022-02-05 19:03:36 +0200

    Updated the app to fix the type of the returned variables
    Return variable cannot be an integer

--- app.py
+++ app.py
@@ -12,13 +12,13 @@ def home():
 def addition(number1, number2):
     """Return a the addition of the two numbers."""
     print("I am inside addition")
-    return number1+number2
+    return str(int(number1)+int(number2))
 
 @app.route("/multi/<number1>/<number2>")
 def multiply(number1, number2):
     """Returns the multiplication of the two numbers."""
     print("I am inside multiplication")
-    return number1*number2
+    return str(int(number1)*int(number2))
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
