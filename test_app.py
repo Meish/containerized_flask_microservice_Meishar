f3dc6fe81ef08478ef52ce2cd080dbf14810b577
Meishar Shahoha <meish123@gmail.com>
2022-02-05 19:04:31 +0200

    Fixed the tests so that will suit the type of the returned variable

--- test_app.py
+++ test_app.py
@@ -4,13 +4,13 @@ def test_home():
     assert('Hello World! I can make addition at route /add' == home())
 
 def test_add():
-    assert(addition(1,2) == 3)
-    assert(addition(3,2) == 5)
-    assert(addition(1,5) == 6)
-    assert(addition(150,-32) == 118)
+    assert(addition(1,2) == '3')
+    assert(addition(3,2) == '5')
+    assert(addition(1,5) == '6')
+    assert(addition(150,-32) == '118')
 
 def test_multiply():
-    assert(multiply(1,2) == 2)
-    assert(multiply(3,2) == 6)
-    assert(multiply(1,5) == 5)
-    assert(multiply(150,0) == 0)
+    assert(multiply(1,2) == '2')
+    assert(multiply(3,2) == '6')
+    assert(multiply(1,5) == '5')
+    assert(multiply(150,0) == '0')
