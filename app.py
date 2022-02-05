e57957d5ce41031136c54fbe79c6fe9aabb461cf
Meishar Shahoha <meish123@gmail.com>
2022-02-05 18:46:46 +0200

    added functions to microservice and updated the test_app accordingly

--- app.py
+++ app.py
@@ -6,7 +6,19 @@ app = Flask(__name__)
 def home():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return 'Hello World! I can make change at route: /change'
+    return 'Hello World! I can make addition at route /add'
+
+@app.route("/add/<number1>/<number2>")
+def addition(number1, number2):
+    """Return a the addition of the two numbers."""
+    print("I am inside addition")
+    return number1+number2
+
+@app.route("/multi/<number1>/<number2>")
+def multiply(number1, number2):
+    """Returns the multiplication of the two numbers."""
+    print("I am inside multiplication")
+    return number1*number2
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
--- test_app.py
+++ test_app.py
@@ -1,4 +1,16 @@
-from app import home
+from app import home, addition, multiply
 
 def test_home():
-    assert('Hello World! I can make change at route: /change' == home())
+    assert('Hello World! I can make addition at route /add' == home())
+
+def test_add():
+    assert(addition(1,2) == 3)
+    assert(addition(3,2) == 5)
+    assert(addition(1,5) == 6)
+    assert(addition(150,-32) == 128)
+
+def test_multiply():
+    assert(multiply(1,2) == 2)
+    assert(multiply(3,2) == 6)
+    assert(multiply(1,5) == 5)
+    assert(multiply(150,0) == 0)
